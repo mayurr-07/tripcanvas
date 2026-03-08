61349ddc87a52c64c9188eac316e035512672d5b
RohitHuge <rohitrhuge@gmail.com>
2026-03-09 00:25:28 +0530

    Refactor

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'https://tripcanvas.pages.dev/',
+app.use(cors({
+    origin: ['https://tripcanvas.pages.dev', 'http://localhost:5173'],
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],}));
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+}));
+
 app.use(express.json());
 
 app.use("/api/attachments", attachmentRoutes);
